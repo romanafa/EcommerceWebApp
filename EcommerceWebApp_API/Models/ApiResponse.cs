b6c4f82825c9c93b02cfb038f0fbceeb19228e24
Romana Faiglova <r.faiglova@gmail.com>
2024-01-29 22:55:14 +0100

    Product: GetAll and GetById

--- EcommerceWebApp_API/Models/ApiResponse.cs
+++ EcommerceWebApp_API/Models/ApiResponse.cs
@@ -1,6 +1,20 @@
-﻿namespace EcommerceWebApp_API.Models
+﻿using System.Net;
+
+namespace EcommerceWebApp_API.Models
 {
+    // Returns a generic response for all API calls
     public class ApiResponse
     {
+        public ApiResponse()
+        {
+            Errors = new List<string>();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> Errors { get; set; }
+
+        // Defined as object to allow for any type of response
+        public object Response { get; set; }
     }
 }
