6e2d8d09702d9b5b689065d8cb478016f89fb9d1
Romana Faiglova <r.faiglova@gmail.com>
2024-01-29 22:07:25 +0100

    Initial seed of products and categories

--- EcommerceWebApp_API/Data/ApplicationDbContext.cs
+++ EcommerceWebApp_API/Data/ApplicationDbContext.cs
@@ -15,12 +15,75 @@ namespace EcommerceWebApp_API.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-        //    // Data seeding
+            // Data seeding
+            //TODO: Seed product categories when API testing is functional 
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = 1,
+                    ProductTitle = "Headband - Easter",
+                    Description = "Handmade headband",
+                    Price = 200.00,
+                    Stock = 10,
+                    Colour = "Multi",
+                    Size = "Onesize",
+                    IsActive = true,
+                    Image = "https://ecommerceproductsimages.blob.core.windows.net/images/IMG_8882.JPG",
+                    CreatedAt = new System.DateTime(2024, 1, 29)
+                },
+                new Product
+                {
+                    ProductId = 2,
+                    ProductTitle = "Headband - Pinky",
+                    Description = "Handmade headband",
+                    Price = 200.00,
+                    Stock = 10,
+                    Colour = "Multi",
+                    Size = "Onesize",
+                    IsActive = true,
+                    Image = "https://ecommerceproductsimages.blob.core.windows.net/images/IMG_8884.JPG",
+                    CreatedAt = new System.DateTime(2024, 1, 29)
+                },
+                new Product
+                {
+                    ProductId = 3,
+                    ProductTitle = "Headband - Summer",
+                    Description = "Handmade headband",
+                    Price = 200.00,
+                    Stock = 10,
+                    Colour = "Multi",
+                    Size = "Onesize",
+                    IsActive = true,
+                    Image = "https://ecommerceproductsimages.blob.core.windows.net/images/IMG_8886.JPG",
+                    CreatedAt = new System.DateTime(2024, 1, 29)
+                });
 
-        //}
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    CategoryId = 1,
+                    CategoryTitle = "Accessories",
+                    IsActive = true
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    CategoryTitle = "Decorations",
+                    IsActive = true
+                },
+                new Category
+                {
+                    CategoryId = 3,
+                    CategoryTitle = "Magnets",
+                    IsActive = true
+                }
+                );
+
+        }
     }
 }
---
+++ EcommerceWebApp_API/Migrations/20240129210518_SeedProductAndCategories.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EcommerceWebApp_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductAndCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryTitle", "IsActive" },
+                values: new object[,]
+                {
+                    { 1, "Accessories", true },
+                    { 2, "Decorations", true },
+                    { 3, "Magnets", true }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Colour", "CreatedAt", "Description", "Image", "IsActive", "Price", "ProductTitle", "Size", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Multi", new DateTime(2024, 1, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), "Handmade headband", "https://ecommerceproductsimages.blob.core.windows.net/images/IMG_8882.JPG", true, 200.0, "Headband - Easter", "Onesize", 10 },
+                    { 2, "Multi", new DateTime(2024, 1, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), "Handmade headband", "https://ecommerceproductsimages.blob.core.windows.net/images/IMG_8884.JPG", true, 200.0, "Headband - Pinky", "Onesize", 10 },
+                    { 3, "Multi", new DateTime(2024, 1, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), "Handmade headband", "https://ecommerceproductsimages.blob.core.windows.net/images/IMG_8886.JPG", true, 200.0, "Headband - Summer", "Onesize", 10 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+        }
+    }
+}
