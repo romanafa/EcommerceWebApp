a2c96e717c38ead4412199c38c34756a75509f7a
Romana Faiglova <r.faiglova@gmail.com>
2024-01-29 09:31:51 +0100

    Connection string setup and initial migration with Identity tables

---
+++ EcommerceWebApp_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceWebApp_API.Data
+{
+
+    // Using IdentityDbContext instead of DbContext as we agreed to use Identity for authentication and authorization
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- EcommerceWebApp_API/Program.cs
+++ EcommerceWebApp_API/Program.cs
@@ -1,7 +1,13 @@
+using EcommerceWebApp_API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
