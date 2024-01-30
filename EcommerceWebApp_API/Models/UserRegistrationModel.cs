0c7ea15c805452843d45dbd4d369ca93d44dcdfa
Austnez <jrgen96@gmail.com>
2024-01-30 22:53:33 +0100

    Basic User functions added

--- EcommerceWebApp_API/Models/UserRegistrationModel.cs
+++ EcommerceWebApp_API/Models/UserRegistrationModel.cs
@@ -10,9 +10,16 @@ namespace EcommerceWebApp_API.Models
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
 
-       
+
+
 
         [Required]
         public string FirstName { get; set; }
