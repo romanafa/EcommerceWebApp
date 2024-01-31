eeaa4206f31cb4086dfb915600d772d6d132ce4a
Romana Faiglova <r.faiglova@gmail.com>
2024-01-31 16:37:51 +0100

    Implemented Blob service for image upload (Azure Storage)

---
+++ EcommerceWebApp_API/Services/IBlobService.cs
@@ -0,0 +1,11 @@
+﻿namespace EcommerceWebApp_API.Services
+{
+    public interface IBlobService
+    {
+        // actions to get/delete/upload the image file
+        Task<string> GetBlob(string blobName, string container);
+        Task<bool> DeleteBlob(string blobName, string container);
+        Task<string> CreateBlob(string blobName, string container, IFormFile image);
+
+    }
+}
