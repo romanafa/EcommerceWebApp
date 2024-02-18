cf03e683b868c4e0ddadf6306b4084d086577bdd
Austnez <jrgen96@gmail.com>
2024-02-18 20:59:56 +0100

    Added Password requirements

--- EcommerceWebApp_API/Models/UserRegistrationModel.cs
+++ EcommerceWebApp_API/Models/UserRegistrationModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
 
 namespace EcommerceWebApp_API.Models
 {
-    public class UserRegistrationModel
+    public class UserRegistrationModel : IValidatableObject
     {
         [Required]
         [EmailAddress]
@@ -12,20 +14,31 @@ namespace EcommerceWebApp_API.Models
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
+
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
-
-
-
         [Required]
         public string FirstName { get; set; }
 
         [Required]
         public string LastName { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$";
+            var regex = new Regex(passwordPattern);
+
+            if (!regex.IsMatch(Password))
+            {
+                yield return new ValidationResult(
+                    "Password must be at least 8 characters long, contain at least one digit, one uppercase letter, one lowercase letter, and one special character.",
+                    new[] { nameof(Password) });
+            }
+        }
     }
 
 }
--- EcommerceWebApp_API/Program.cs
+++ EcommerceWebApp_API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 // Connection string for azure image storage
 builder.Services.AddSingleton(x => new BlobServiceClient(
-    builder.Configuration.GetConnectionString("StorageAccount")));
+   builder.Configuration.GetConnectionString("StorageAccount")));
 builder.Services.AddSingleton<IBlobService, BlobService>();
 
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
